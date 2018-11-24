c75d168c66ecf342dab8b3b6b4e0098757cb9cfe
Michelle <mich.luo@mail.utoronto.ca>
2018-11-24 07:39:42 -0500

    Can open and take a picture with camera

--- HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
+++ HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
@@ -1,7 +1,11 @@
 package com.example.montana.hackathonproject;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,9 +15,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Profilestats extends AppCompatActivity {
 
     private AnimationDrawable animation;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +36,20 @@ public class Profilestats extends AppCompatActivity {
         imView.setBackgroundResource(R.drawable.animation);
         animation = (AnimationDrawable) imView.getBackground();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addFoodButton);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addWorkoutButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        FloatingActionButton camBtn = (FloatingActionButton) findViewById(R.id.addFoodButton);
+        camBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
             }
         });
     }
@@ -62,5 +80,49 @@ public class Profilestats extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            onActivityResult(REQUEST_IMAGE_CAPTURE, RESULT_OK, takePictureIntent);
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                //galleryAddPic(photoFile);
+            } catch (IOException ex) {
+
+            }
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    //for saving to phone gallery, not used yet
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
