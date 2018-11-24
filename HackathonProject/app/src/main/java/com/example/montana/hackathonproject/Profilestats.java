65b939c26ac7b889884af32751c7591630368238
Yimin Xu <yxu537@uwo.ca>
2018-11-24 06:36:39 -0500

    Facebook login: Showing name text

--- HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
+++ HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
@@ -13,11 +13,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
@@ -28,6 +31,8 @@ public class Profilestats extends AppCompatActivity {
     private AnimationDrawable animation;
     private CallbackManager callbackManager;
     private Button login_button;
+    private ProfileTracker profileTracker;
+    private TextView text_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,16 +54,44 @@ public class Profilestats extends AppCompatActivity {
             }
         });
 
+        text_name = findViewById(R.id.text_name);
+
+        //Facebook Login
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
 
         login_button = findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+                if (currentProfile != null)
+                {
+                    String name = currentProfile.getFirstName() + " " + currentProfile.getLastName();
+                    text_name.setText(name);
+                    text_name.setVisibility(View.VISIBLE);
+                }else
+                    text_name.setVisibility(View.INVISIBLE);
+            }
+        };
+
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+
                         Log.d("FacebookLogin","Logged In Successfully");
+
+                        if (Profile.getCurrentProfile() != null)
+                        {
+                            String name = Profile.getCurrentProfile().getFirstName() + " " + Profile.getCurrentProfile().getLastName();
+                            text_name.setText(name);
+                            text_name.setVisibility(View.VISIBLE);
+                        }else
+                            text_name.setVisibility(View.INVISIBLE);
                     }
 
                     @Override
@@ -72,6 +105,8 @@ public class Profilestats extends AppCompatActivity {
                         Log.d("FacebookLogin",exception.toString());
                     }
                 });
+
+
     }
 
     @Override
@@ -107,4 +142,11 @@ public class Profilestats extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
 }
