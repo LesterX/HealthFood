c59741ab040f411602b827a025b8b0cee5c399e4
Yimin Xu <yxu537@uwo.ca>
2018-11-24 04:51:48 -0500

    Facebook login button

--- HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
+++ HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
@@ -1,13 +1,14 @@
 package com.example.montana.hackathonproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Profilestats extends AppCompatActivity {
 
@@ -48,5 +49,11 @@ public class Profilestats extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Onclick function for login page
+    public void loginActivity(View v) {
+        Intent intent = new Intent(Profilestats.this,Login.class);
+        startActivity(intent);
     }
 }
