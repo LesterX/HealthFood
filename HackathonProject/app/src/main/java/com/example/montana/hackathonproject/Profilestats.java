3226d42dfc4187aef1daa3438324d729be2af08a
Michelle <mich.luo@mail.utoronto.ca>
2018-11-24 04:41:09 -0500

    Animation works

--- HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
+++ HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
@@ -1,15 +1,19 @@
 package com.example.montana.hackathonproject;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Profilestats extends AppCompatActivity {
+
+    private AnimationDrawable animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,11 @@ public class Profilestats extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ImageView imView = (ImageView) findViewById(R.id.avatarImageView);
+        imView.setBackgroundResource(R.drawable.animation);
+        animation = (AnimationDrawable) imView.getBackground();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addFoodButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +34,12 @@ public class Profilestats extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        this.animation.start();
     }
 
     @Override
