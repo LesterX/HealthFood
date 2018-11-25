b29ed393a994af1b18856af7fd6af96d9aa60645
Michelle <mich.luo@mail.utoronto.ca>
2018-11-25 01:55:11 -0500

    Animation complete

--- HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
+++ HackathonProject/app/src/main/java/com/example/montana/hackathonproject/Profilestats.java
@@ -1,9 +1,10 @@
 package com.example.montana.hackathonproject;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -29,7 +30,6 @@ import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-
 
 import java.io.File;
 import java.io.IOException;
@@ -57,18 +57,8 @@ public class Profilestats extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ImageView imView = (ImageView) findViewById(R.id.avatarImageView);
-        imView.setBackgroundResource(R.drawable.animation);
+        imView.setBackgroundResource(R.drawable.animationhappy);
         animation = (AnimationDrawable) imView.getBackground();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addWorkoutButton);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setActioI feel("Action", null).show();*/
-
-            }
-        });
 
 
         FloatingActionButton camBtn = (FloatingActionButton) findViewById(R.id.addFoodButton);
@@ -139,7 +129,7 @@ public class Profilestats extends AppCompatActivity {
                 });
 
 
-        new testTask().execute(".");
+        //new testTask().execute(".");
 
         //Workout Progress Bar
         TextView status = (TextView)findViewById(R.id.workoutProgressView);
@@ -160,26 +150,37 @@ public class Profilestats extends AppCompatActivity {
         Boolean Happy =false;
         Boolean Sick= false;
         Boolean Hungry=false;
+        ImageView vw;
 
         if (foodbar.getProgress()<=1000 && workoutbar.getProgress()<=29){
             Sick=true;
+            vw = (ImageView) findViewById(R.id.avatarImageView);
+            vw.setBackgroundResource(R.drawable.animationsad);
             happylvl.setProgress(20);
+            happylvl.setProgressTintList(ColorStateList.valueOf(Color.RED));
 
         }
         else if (foodbar.getProgress()<=1000 && workoutbar.getProgress()>=29){
             Hungry=true;
+            vw = (ImageView) findViewById(R.id.avatarImageView);
+            vw.setBackgroundResource(R.drawable.animationhungry);
             happylvl.setProgress(60);
+            happylvl.setProgressTintList(ColorStateList.valueOf(Color.YELLOW));
         }
         else {
             Happy=true;
+            vw = (ImageView) findViewById(R.id.avatarImageView);
+            vw.setBackgroundResource(R.drawable.animationhappy);
             happylvl.setProgress(80);
+            happylvl.setProgressTintList(ColorStateList.valueOf(Color.GREEN));
         }
+        animation = (AnimationDrawable) vw.getBackground();
 
     }
 
 
 
-
+/*
     //Testing task
     private class testTask extends AsyncTask<String,Integer,String>{
         protected String doInBackground(String... params) {
@@ -187,7 +188,7 @@ public class Profilestats extends AppCompatActivity {
             recog.readImage("https://i5.walmartimages.ca/images/Large/580/6_r/875806_R.jpg");
             //FoodNutrition nutri = new FoodNutrition();
 
-            /*
+            *//*
             if (recog.getTopResult() != null) {
                 Map<String, Float> nutrition_list = nutri.getNutrition(recog.getTopResult());
                 Log.d("ImageTest",recog.getTopResult());
@@ -197,13 +198,13 @@ public class Profilestats extends AppCompatActivity {
                     Log.d("ImageTest", "Food not found");
             }else
                 Log.d("ImageTest","Image not found");
-            */
+            *//*
 
             publishProgress(0);
 
             return "Test complete";
         }
-    }
+    }*/
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
